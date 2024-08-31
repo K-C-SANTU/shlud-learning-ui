4a9a21cab9099d991ef5bcac2258487b782a81bb
Shruti <shrutilg3101998@gmail.com>
2024-08-31 10:20:22 +0530

    changes in Greet

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -5,6 +5,14 @@ import React from "react"
 //     return <h1>Hello Santosh</h1>
 // }
 
-const Greet=()=><h1>Functional Component</h1>
+const Greet = () => {
+    return (
+        <>
+            <h1>Functional Component</h1>
+            <h2>Shruti</h2>
+        </>
+    )
+}
 
-export default Greet
+
+export default Greet;
