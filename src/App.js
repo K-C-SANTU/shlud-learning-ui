34641493e967c6f80c6648678ba156b112ffdd52
KC Santosh <santosh97312@gmail.com>
2024-08-31 09:31:11 +0530

    Added some text to the app page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Components/Login';
 function App() {
   return (
     <div className='App'> 
+     my name is kc
     <Greet></Greet>
     <Welcome></Welcome>
     <Login></Login>
