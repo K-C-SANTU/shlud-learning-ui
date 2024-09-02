6d41fcf536fc37b4ee68d5112e263984afd6ed34
Shruti <shrutilg3101998@gmail.com>
2024-09-02 15:05:53 +0530

    UseStateHook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './Components/Greet';
-import Welcome from './Components/welcome';
-import Login from './Components/Login';
+import { Button, Stack } from "@mui/material";
+import UseStateHook from "./ReactHooks/UseState.hook";
+// handling promisses 
+let interval;
+const App = () => {
+  const testing = async () => {
+    // console.log("1");
+    // await fetch('https://dummyjson.com/users').then((res)=>{
+    //   console.log(2);
+    // })
+    // console.log(3);
 
-function App() {
+    // const timeOut =  setTimeout(()=>{
+    //   console.log("4")
+    // },3000)
+
+    // clearTimeout(timeOut);
+
+    let count = 1;
+    clearInterval(interval);
+    interval = setInterval(()=>{
+      console.log("interval ", count++);
+    },1000)
+    // clearInterval(interval);
+    // console.log("5")
+
+  };
+
   return (
-    <div className='App'> 
-     my name is shruti Gataraddihal Kc santosh
-    <Greet></Greet>
-    <Welcome></Welcome>
-    <Login></Login>
-    </div>
+    <Stack spacing={2} direction="column">
+      {/* <Button variant="outlined" onClick={testing}>
+        testing
+      </Button> */}
+      <UseStateHook />
+      
+    </Stack>
   );
 }
 
