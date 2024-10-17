fc9be6d72d817b9aa1c184c7c01212cd0fdcb51c
KC Santosh <santosh97312@gmail.com>
2024-10-17 12:05:32 +0530

    FIX

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import Profile from './Profile';
+// import Profile from './Profile';
 import { UseContextProvider } from './Routes';
 
 function Header() {
